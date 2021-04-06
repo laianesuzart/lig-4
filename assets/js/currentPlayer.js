46bbf2abe8653f4e67136f77103f380ca41c1936
Gustavo Henrique Cielo <gh.cielo@hotmail.com>
2021-04-06 18:50:35 +0000

    Logical

---
+++ assets/js/currentPlayer.js
@@ -0,0 +1,18 @@
+currentPlayerName1 = "Naruto"
+currentPlayerName2 = "Sakura"
+let currentPlayerName = ""
+const gameTableID = document.getElementById("gameTable");
+const showPlayer = document.createElement("span")
+showPlayer.innerText = currentPlayerName
+gameTableID.appendChild(showPlayer);
+
+document.addEventListener("click", () => {
+    if (currentPlayer === "player1") {
+        currentPlayerName = currentPlayerName1
+    }
+    if (currentPlayer === "player2") {
+        currentPlayerName = currentPlayerName2
+    }
+    showPlayer.innerText = currentPlayerName
+    gameTableID.appendChild(showPlayer);
+})
