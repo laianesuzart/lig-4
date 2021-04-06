42de6f61d4fa2b95be7563632b2946a833737f92
Laiane Suzart <laiane@pm.me>
2021-04-06 17:08:57 -0300

    Add village's history

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -10,6 +10,11 @@ rulesBtn.addEventListener('click', () => {
     popUp.classList.toggle('hidden');
 });
 
+historyBtn.addEventListener('click', () => {
+    text.innerText = 'Após a Grande Guerra Ninja, a vila de Konoha ficou devastado. Mas, graças ao grande Hokage Howard, foi possível recontruir a vila em poucos meses. Além disso, sua incrível atuação na Guerra Ninja possibilitou salvar milhares de vidas, seu ninjutsu estilo água: Hsksjdh no Jutsu destruiu inimigos. Os habitantes de Konoha são tão gratos ao Hokage Howard que decidiram renomear sua vila para Howard Village.';
+    popUp.classList.toggle('hidden');
+});
+
 exitBtn.addEventListener('click', () => {
     popUp.classList.add('hidden');
 });
